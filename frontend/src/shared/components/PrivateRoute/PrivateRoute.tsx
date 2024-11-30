62b5753d076869b47d1f7dfa5d57c7cb24afbc04
Ilya Sinyakin <arqu1t@mail.ru>
2024-11-30 15:47:49 +0300

    Private route

--- frontend/src/shared/components/PrivateRoute/PrivateRoute.tsx
+++ frontend/src/shared/components/PrivateRoute/PrivateRoute.tsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import { type FC, type PropsWithChildren } from "react";
+import { Navigate } from "react-router-dom";
+import { useAuthToken } from "@hooks/useAuthToken";
 
-export const PrivateRoute = () => {
-  return (
-    <div>PrivateRoute</div>
-  )
-}
+export const PrivateRoute: FC<PropsWithChildren> = ({ children }) => {
+  const { getAccessToken } = useAuthToken();
+
+  if (!getAccessToken()) {
+    return <Navigate to="sign-in" />;
+  }
+
+  return children;
+};
