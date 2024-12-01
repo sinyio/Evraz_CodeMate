0ef587f2145dd54f7e113bbea08195c590b8335e
Ilya Sinyakin <arqu1t@mail.ru>
2024-12-01 08:58:20 +0300

    fix file

--- frontend/src/modules/UploadFileForm/api/useUploadFile.tsx
+++ frontend/src/modules/UploadFileForm/api/useUploadFile.tsx
@@ -6,6 +6,7 @@ export const useUploadFile = () => {
   const { mutateAsync, isError, isSuccess, isPending } = useMutation({
     mutationFn: async ({ file }: { file: File }) => {
       const response = await uploadFileApi.uploadFile(file);
+      console.log(response);
       return response;
     },
     onSettled: () => showFormStore.hideForm(),
@@ -13,16 +14,14 @@ export const useUploadFile = () => {
 
   const uploadFile = async (file: File) => {
     const response = await mutateAsync({ file });
-
-    if (response) {
-      const url = window.URL.createObjectURL(response);
+    if (response?.data) {
+      const url = window.URL.createObjectURL(new Blob([response.data]));
       const link = document.createElement("a");
       link.href = url;
       link.setAttribute("download", "Отчет.pdf");
       document.body.appendChild(link);
       link.click();
       link.parentNode.removeChild(link);
-      window.URL.revokeObjectURL(url);
     }
   };
 
