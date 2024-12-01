e30cc138f9a1849e868d4522f7a7c8c8f10a460d
Ilya Sinyakin <arqu1t@mail.ru>
2024-12-01 09:09:04 +0300

    cors

--- backend/project/project/settings.py
+++ backend/project/project/settings.py
@@ -48,7 +48,10 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://77.223.98.30:5173"
+]
 
 ROOT_URLCONF = "project.urls"
 
