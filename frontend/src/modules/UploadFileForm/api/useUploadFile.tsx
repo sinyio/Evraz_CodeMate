4bfba7ec62949f9aeac193b525d945bd4fbfc17b
Ilya Sinyakin <arqu1t@mail.ru>
2024-12-01 08:50:01 +0300

    fix file extension

--- frontend/src/modules/UploadFileForm/api/useUploadFile.tsx
+++ frontend/src/modules/UploadFileForm/api/useUploadFile.tsx
@@ -6,7 +6,6 @@ export const useUploadFile = () => {
   const { mutateAsync, isError, isSuccess, isPending } = useMutation({
     mutationFn: async ({ file }: { file: File }) => {
       const response = await uploadFileApi.uploadFile(file);
-      console.log(response);
       return response;
     },
     onSettled: () => showFormStore.hideForm(),
@@ -14,14 +13,16 @@ export const useUploadFile = () => {
 
   const uploadFile = async (file: File) => {
     const response = await mutateAsync({ file });
-    if (response?.data) {
-      const url = window.URL.createObjectURL(new Blob([response.data]));
+
+    if (response) {
+      const url = window.URL.createObjectURL(response);
       const link = document.createElement("a");
       link.href = url;
       link.setAttribute("download", "Отчет.pdf");
       document.body.appendChild(link);
       link.click();
       link.parentNode.removeChild(link);
+      window.URL.revokeObjectURL(url);
     }
   };
 
